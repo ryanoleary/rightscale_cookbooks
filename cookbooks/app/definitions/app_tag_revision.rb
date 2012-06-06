719c617112d1eb6e66907ce8b04808ec66c6eca1
Ryan OLeary <ryan.oleary@rightscale.com>
2012-06-06 16:18:15 -0700

    Changed to use node instead of new_resource

--- cookbooks/app/definitions/app_tag_revision.rb
+++ cookbooks/app/definitions/app_tag_revision.rb
@@ -1,7 +1,7 @@
 define :app_tag_revision do
   
   # Set the revision tag of the server
-  revision_tag = "appserver:app_revision=#{new_resource.revision}"
+  revision_tag = "appserver:app_revision=#{node[:repo][:default][:revision]}"
   right_link_tag revision_tag  
 
 
