f317675d98cb2c89531978d11f58ae555720085d
Ryan O'Leary <ryan.oleary@rightscale.com>
2013-07-17 08:20:28 -0700

    Added new cookbook

---
+++ cookbooks/lb_rol/metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          "Copyright RightScale, Inc. All rights reserved."
+description      "DOES NOTHING"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "12.1.0"
+
+supports "centos", "~> 5.8"
+supports "redhat", "~> 5.8"
+supports "ubuntu", "~> 10.04.0"
+
+depends "rightscale"
+depends "lb"
